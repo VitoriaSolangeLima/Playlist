c3785c1155a2440bf57af030a01f2a07c026c9bf
Vitória Solange <152615603+VitoriaSolangeLima@users.noreply.github.com>
2026-07-17 14:25:01 +0000

    configuração da api

--- Music/Program.cs
+++ Music/Program.cs
@@ -2,8 +2,26 @@ using Music.Models;
 using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+// Cria um objeto responsável por configurar a aplicação ASP.NET
+var builder = WebApplication.CreateBuilder(args);
 
+// Define o endereço e a porta em que a aplicação irá escutar requisições HTTP
+builder.WebHost.UseUrls("http://localhost:8000");
 
+// Constrói a aplicação web a partir das configurações definidas no builder
+var app = builder.Build();
+
+Musica[] listamusicas = new Musicas[100];
+int totalmusicas = 0;
+
+// Definição de rotas HTTP do tipo GET 
+app.MapPost("/Musica", (JasonElement body ) => 
+{
+    Random random = new();
+    Musica[] musica = new 
+
+});
+
+// Inicia o servidor web é iniciado e passa a aguardar requisições HTTP dos clientes
 app.Run();
 
