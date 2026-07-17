7e4d4c02bedfeb3f863cf62335b3b274692845e6
Vitória Solange <152615603+VitoriaSolangeLima@users.noreply.github.com>
2026-07-17 14:15:08 +0000

    jdhcsf

---
+++ Music/Models/Musica.cs
@@ -0,0 +1,11 @@
+namespace Music.Models;
+public class Musica
+{
+    public int id { get; set; }
+    public String Titulo { get; set; }
+    public String Compositor { get; set; }
+    public String Artista { get; set; }
+    public String Genero { get; set; }
+    public int Ano { get; set; }
+
+}
---
+++ Music/Program.cs
@@ -0,0 +1,9 @@
+using Music.Models;
+using System.Text.Json;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+
+app.Run();
+
