56861191f25151f171cf2df64f7ca0b07009bff9
Vitória Solange <152615603+VitoriaSolangeLima@users.noreply.github.com>
2026-07-23 11:29:17 +0000

    Só pra ter certeza que mandei tudo

--- Music/obj/Debug/net10.0/Music.AssemblyInfo.cs
+++ Music/obj/Debug/net10.0/Music.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Music")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47171989ff7df05b3a5466b2c0d00f07105785bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d479640487af44a7f034364b98e802e66dcc7160")]
 [assembly: System.Reflection.AssemblyProductAttribute("Music")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Music")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
